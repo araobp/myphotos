c7a8d803133471e8ef5b18c0ed10da0afe9b5582
araobp <araobp@users.noreply.github.com>
2022-08-27 21:25:25 +0900

    update

---
+++ myphotos/myphotos/main/default/lwc/imageViewer/imageViewer.js
@@ -0,0 +1,51 @@
+import { LightningElement, api, wire } from 'lwc';
+import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
+import RECORD_ID_UPDATE_MESSAGE from '@salesforce/messageChannel/RecordId__c';
+import getImageURL from '@salesforce/apex/ImageURL.getImageURL';
+
+export default class PhotoViewer extends LightningElement {
+  @api recordId;
+  showModal = false;
+  imageURL = null;
+
+  subscription = null;
+
+  @wire(MessageContext)
+  messageContext;
+
+  connectedCallback() {
+    this.subscription = subscribe(
+      this.messageContext,
+      RECORD_ID_UPDATE_MESSAGE,
+      (message) => {
+        this.recordId = message.recordId;
+        this.loadImage();
+      }
+    );
+    this.loadImage();
+  }
+
+  disconnectedCallback() {
+    if (this.subscription != null) {
+      unsubscribe(this.subscription);
+      this.subscription = null;
+    }
+  }
+
+  loadImage() {
+    console.log('recordId: ' + this.recordId);
+    getImageURL({recordId: this.recordId})
+    .then(url => {
+      //console.log('imageURL: ' + url);
+      this.imageURL = url;
+    });
+  }
+
+  enableModal() {
+    this.showModal = true;
+  }
+
+  disableModal() {
+    this.showModal = false;
+  }
+}
