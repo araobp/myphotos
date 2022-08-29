8b0ed9c4fbd1878ba50992544c8e0e66e7a95aa5
araobp <araobp@users.noreply.github.com>
2022-08-29 22:40:21 +0900

    Update webCam.js

--- myphotos/myphotos/main/default/lwc/webCam/webCam.js
+++ myphotos/myphotos/main/default/lwc/webCam/webCam.js
@@ -32,8 +32,42 @@ export default class WebCam extends LightningElement {
     const f = e.target.files[0];
     const reader = new FileReader();
     reader.onload = _ => {
-        this.imageURL = reader.result;
+      const imageURL = reader.result;
+      console.log(imageURL);
+      this.resizeImage(imageURL, 128, resizedImageURL => {
+        console.log(resizedImageURL);
+        this.imageURL = resizedImageURL;  
+      });
     };
     reader.readAsDataURL(f);
+  }
+
+  dataURItoBlob = async dataURI => {
+    return await fetch(dataURI).then(r => r.blob());
+  }
+
+  resizeImage = (imageURL, targetWidth, callback) => {
+    const img = document.createElement('img');
+    img.addEventListener('load', function() {
+      const width = img.width;
+      const height = img.height;
+      console.log('width: ' + width + ', height: ' + height);
+
+      const canvas = document.createElement('canvas');
+
+      if (height > width) {
+        canvas.width = targetWidth;
+        canvas.height = height * targetWidth / width;  
+      } else {
+        canvas.height = targetWidth;
+        canvas.width = width * targetWidth / height;  
+      }
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      const resizedImageURL = canvas.toDataURL('image/jpeg');
+      callback(resizedImageURL);
+    });
+    img.src = imageURL;
   }
 }
