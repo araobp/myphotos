cfb16e726f62fd8f0be07c2cbeca9a5046364f7a
araobp <araobp@users.noreply.github.com>
2022-09-23 02:55:23 +0900

    update

--- myphotos/myphotos/main/default/lwc/placeQuickAction/placeQuickAction.js
+++ myphotos/myphotos/main/default/lwc/placeQuickAction/placeQuickAction.js
@@ -1,35 +1,27 @@
 import { LightningElement, api, wire } from 'lwc';
+import addPlace from '@salesforce/apex/RecordObject.addPlace';
+import { CloseActionScreenEvent } from 'lightning/actions';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import RECORD_NAME_FIELD from '@salesforce/schema/Record__c.Name';
 import RECORD_ADDRESS_FIELD from '@salesforce/schema/Record__c.Address__c';
-import addPlace from '@salesforce/apex/RecordObject.addPlace';
-import { CloseActionScreenEvent } from 'lightning/actions';
 
-const RECORD_GEOLOCATION_LATITUDE_FIELD = 'Record__c.Geolocation__Latitude__s';
-const RECORD_GEOLOCATION_LONGITUDE_FIELD = 'Record__c.Geolocation__Longitude__s';
-
-const recordFields = [
-  RECORD_NAME_FIELD,
-  RECORD_ADDRESS_FIELD,
-  RECORD_GEOLOCATION_LATITUDE_FIELD,
-  RECORD_GEOLOCATION_LONGITUDE_FIELD
-];
+const recordFields = [ RECORD_NAME_FIELD, RECORD_ADDRESS_FIELD ];
 
 export default class PlaceQuickAction extends LightningElement {
   @api recordId;
 
   name;
   address;
-  latitude;
-  longitude;
   result;
 
   @wire(getRecord, { recordId: '$recordId', fields: recordFields })
   loadRecord({ error, data }) {
-    this.name = getFieldValue(data, RECORD_NAME_FIELD);
-    this.address = getFieldValue(data, RECORD_ADDRESS_FIELD);
-    this.latitude = getFieldValue(data, RECORD_GEOLOCATION_LATITUDE_FIELD);
-    this.longitude = getFieldValue(data, RECORD_GEOLOCATION_LONGITUDE_FIELD);
+    if (error) {
+      console.log(error);
+    } else if (data) {
+      this.name = getFieldValue(data, RECORD_NAME_FIELD);
+      this.address = getFieldValue(data, RECORD_ADDRESS_FIELD);
+    }
   }
 
   handleCancel() {
@@ -37,7 +29,7 @@ export default class PlaceQuickAction extends LightningElement {
   }
 
   handleAdd() {
-    addPlace({ name: this.name, address: this.address, latitude: this.latitude, longitude: this.longitude })
+    addPlace({ recordId: this.recordId })
       .then(success => {
         //this.dispatchEvent(new CloseActionScreenEvent());
         if (success) {
