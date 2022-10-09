c9ed8a089d0292391e810d24126b774a3e01a6b0
araobp <araobp@users.noreply.github.com>
2022-10-09 10:18:14 +0900

    update

---
+++ myphotos/myphotos/main/default/lwc/addPlace/addPlace.js
@@ -0,0 +1,50 @@
+import { LightningElement, api, wire } from 'lwc';
+import addPlace from '@salesforce/apex/RecordObject.addPlace';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import RECORD_NAME_FIELD from '@salesforce/schema/Record__c.Name';
+import RECORD_ADDRESS_FIELD from '@salesforce/schema/Record__c.Address__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const recordFields = [RECORD_NAME_FIELD, RECORD_ADDRESS_FIELD];
+
+export default class AddPlace extends LightningElement {
+
+  @api recordId;
+
+  name = null;
+  address;
+
+  @wire(getRecord, { recordId: '$recordId', fields: recordFields })
+  loadRecord({ error, data }) {
+    if (error) {
+      console.log(error);
+    } else if (data) {
+      this.name = getFieldValue(data, RECORD_NAME_FIELD);
+      this.address = getFieldValue(data, RECORD_ADDRESS_FIELD);
+    }
+  }
+
+  handleAddPlace() {
+    addPlace({ recordId: this.recordId })
+      .then(success => {
+        this.template.querySelector('lightning-button').blur();
+        if (success) {
+          this.dispatchEvent(
+            new ShowToastEvent({
+              title: 'Success',
+              message: 'Place added',
+              variant: 'success'
+            })
+          );
+        } else {
+          this.dispatchEvent(
+            new ShowToastEvent({
+              title: 'Duplicated',
+              message: 'Place not added',
+              variant: 'warning'
+            })
+          );
+        }
+      });
+  }
+}
--- myphotos/myphotos/main/default/lwc/taskHere/taskHere.js
+++ myphotos/myphotos/main/default/lwc/taskHere/taskHere.js
@@ -1,44 +1,25 @@
-import { LightningElement, wire } from 'lwc';
-import { GPS } from 'c/gps';
+import { LightningElement, api } from 'lwc';
 import findTasksNearby from '@salesforce/apex/RecordObject.findTasksNearby';
 import makeTasksCompleted from '@salesforce/apex/RecordObject.makeTasksCompleted';
-import { publish, MessageContext } from 'lightning/messageService';
-import RECORD_ID_UPDATE_MESSAGE from '@salesforce/messageChannel/RecordId__c';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class TaskHere extends LightningElement {
 
-  positioning = true;
+  @api recordId
+
   showTasks = false;
 
-  place = "<Unknown>"
   tasks;
 
-  gps;
-
-  @wire(MessageContext)
-  messageContext;
-
-  constructor() {
-    super();
-    this.gps = new GPS();
-  }
-
   connectedCallback() {
-    this.gps.getGeoLocation(() => {  // Just for updating my current location
-      this.positioning = false;
-      findTasksNearby().
-        then(nearby => {
-          console.log(nearby);
-          this.place = nearby.place;
-          this.tasks = nearby.tasks;
-          console.log({ recordId: this.place.Id });
-          publish(this.messageContext, RECORD_ID_UPDATE_MESSAGE, { recordId: this.place.Id });
-          if (this.tasks.length > 0) {
-            this.showTasks = true;
-          }
-        });
-    });
+    findTasksNearby({ recordId: this.recordId }).
+      then(tasks => {
+        this.tasks = tasks;
+        console.log(tasks);
+        if (this.tasks.length > 0) {
+          this.showTasks = true;
+        }
+      });
   }
 
   handleMakeComplete() {
@@ -54,11 +35,9 @@ export default class TaskHere extends LightningElement {
     if (recordIds.length > 0) {
       makeTasksCompleted({ recordIds })
         .then(() => {
-          findTasksNearby().
-            then(nearby => {
-              console.log(nearby);
-              this.place = nearby.place;
-              this.tasks = nearby.tasks;
+          findTasksNearby({ recordId: this.recordId }).
+            then(tasks => {
+              this.tasks = tasks;
               this.dispatchEvent(
                 new ShowToastEvent({
                   title: 'Success',
