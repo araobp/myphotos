10f0a8fb61174e410b598cb8b498b5ae30a1c426
araobp <araobp@users.noreply.github.com>
2022-10-16 13:16:27 +0900

    update

---
+++ vue/training/main.js
@@ -0,0 +1,72 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'Hello React!'
+  }
+});
+
+const app3 = new Vue({
+  el: '#app3',
+  data: {
+    message_en: 'Hello!',
+    message_ja: 'こんにちは！',
+    lang: 'ja'
+  }
+});
+
+const app4 = new Vue({
+  el: '#app4',
+  data: {
+    message: 'めんそーれ！'
+  }
+});
+
+const app5 = new Vue({
+  el: '#app5',
+  data: {
+    pSize: '40px'
+  }
+});
+
+const app6 = new Vue({
+  el: '#app6',
+  data: {
+   isCapital: true 
+  }
+});
+
+const app7 = new Vue({
+  el: '#app7',
+  data: {
+    products: [
+      {code: '001', name: 'Product001'},
+      {code: '002', name: 'Product002'},
+      {code: '003', name: 'Product003'}
+    ]
+  }
+});
+
+const app8 = new Vue({
+  el: '#app8',
+  data: {
+    price: 980
+  }
+});
+
+const app9 = new Vue({
+  el: '#app9',
+  data: {
+    products: [
+      {code: '001', name: 'Product001'},
+      {code: '002', name: 'Product002'},
+      {code: '003', name: 'Product003'}
+    ]
+  }
+});
