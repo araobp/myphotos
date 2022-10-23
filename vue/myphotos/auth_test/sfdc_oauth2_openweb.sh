d08bef14336cfacc1c94e04c70f79d7850684eb1
araobp <araobp@users.noreply.github.com>
2022-10-23 11:44:59 +0900

    Update sfdc_oauth2_openweb.sh

--- vue/myphotos/auth_test/sfdc_oauth2_openweb.sh
+++ vue/myphotos/auth_test/sfdc_oauth2_openweb.sh
@@ -1,1 +1,1 @@
-open "https://ap26.salesforce.com/services/oauth2/authorize?response_type=code&client_id=$SFDC_CONSUMER_KEY&redirect_uri=$SFDC_REDIRECT_URI"
+open "https://$SFDC_INSTANCE.salesforce.com/services/oauth2/authorize?response_type=code&client_id=$SFDC_CONSUMER_KEY&redirect_uri=$SFDC_REDIRECT_URI"
